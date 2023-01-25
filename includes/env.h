cc8451e2a899fff2fdeb71194b840f19388cd833
Miguel Silva <miguel1998282@gmail.com>
2023-01-25 22:34:20 +0000

    Update env.h

--- includes/env.h
+++ includes/env.h
@@ -6,7 +6,7 @@
 /*   By: mtavares <mtavares@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 01:11:48 by mtavares          #+#    #+#             */
-/*   Updated: 2023/01/25 16:36:28 by mtavares         ###   ########.fr       */
+/*   Updated: 2023/01/25 22:32:16 by mtavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 # define ENV_H
 
 # include "allocs.h"
+# include "cmd.h"
 
 typedef struct s_env		t_env;
 
 struct s_env
 {
-	char	**env;
-	int		**pipe;
-	int		status;
+	char			**env;
+	unsigned char	status;
 };
 
 t_env	*this_env(void);
